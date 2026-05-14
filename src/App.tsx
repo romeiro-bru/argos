6876232bd75197e49d0307cd25061319f2f63f1d
Bruna Romeiro <bruna_romeiro@yahoo.com.br>
2026-05-14 17:18:44 -0300

    init project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+export default function App() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">
+        Projeto iniciado 🚀
+      </h1>
+    </main>
+  );
+}
