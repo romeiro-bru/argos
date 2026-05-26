4d7889997a936fa7a850c42bfbf1a2e9802555b4
Bruna Romeiro <bruna_romeiro@yahoo.com.br>
2026-05-25 22:13:57 -0300

    fix import de imagem

--- src/components/noData.tsx
+++ src/components/noData.tsx
@@ -6,10 +6,10 @@ interface NoDataProps {
 export function NoData({ text, supportText }: NoDataProps) {
   return (
     <div className="flex flex-col text-[var(--gray)] items-center md:translate-y-30">
-      <div className="rounded-full bg-[#fefefe14] p-8 p-4shadow-[var(--shadow)] shadow-lg mb-4">
+      <div className="rounded-full bg-[#fefefe14] p-8 shadow-[var(--shadow)] shadow-lg mb-4">
         <img
           className="w-30 lg:w-50 border-color-[var(--border)] border-rounded"
-          src="sleep.png"
+          src="/sleep.png"
           alt="dog sleeping"
         />
       </div>
