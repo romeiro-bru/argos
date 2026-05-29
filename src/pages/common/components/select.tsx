54a217f08ea0ec7463c82f56d9d402bdd7a9cdb6
Bruna Romeiro <bruna_romeiro@yahoo.com.br>
2026-05-29 17:03:30 -0300

    add props disabled em componente select

--- src/pages/common/components/select.tsx
+++ src/pages/common/components/select.tsx
@@ -2,15 +2,16 @@ interface SelectProps {
   label: string;
   options: { label: string; value: string }[];
   onChange: (value: string) => void;
+  disabled?: boolean
 }
 
-export function Select({ label, options, onChange }: SelectProps) {
+export function Select({ label, options, onChange, disabled }: SelectProps) {
   return (
     <fieldset className="flex flex-col gap-2">
       <label className="font-semibold" htmlFor="state">
         {label}
       </label>
-      <select onChange={(e) => onChange(e.target.value)} id="state" className="text-sm">
+      <select disabled={disabled} onChange={(e) => onChange(e.target.value)} id="state" className="text-sm">
         {options.map((option) => (
           <option key={option.value} value={option.value}>{option.label}</option>
         ))}
