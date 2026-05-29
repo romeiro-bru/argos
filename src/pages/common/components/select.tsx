0b63fd66d373da070ce19842732e19507815e9b9
Bruna Romeiro <bruna_romeiro@yahoo.com.br>
2026-05-29 17:23:19 -0300

    muda estilo ao desabilitar

--- src/pages/common/components/select.tsx
+++ src/pages/common/components/select.tsx
@@ -2,18 +2,25 @@ interface SelectProps {
   label: string;
   options: { label: string; value: string }[];
   onChange: (value: string) => void;
-  disabled?: boolean
+  disabled?: boolean;
 }
 
 export function Select({ label, options, onChange, disabled }: SelectProps) {
   return (
     <fieldset className="flex flex-col gap-2">
-      <label className="font-semibold" htmlFor="state">
+      <label className="font-semibold" htmlFor={label}>
         {label}
       </label>
-      <select disabled={disabled} onChange={(e) => onChange(e.target.value)} id="state" className="text-sm">
+      <select
+        disabled={disabled}
+        onChange={(e) => onChange(e.target.value)}
+        id={label}
+        className={`text-sm ${disabled ? "text-gray-300! cursor-not-allowed" : ""}`}
+      >
         {options.map((option) => (
-          <option key={option.value} value={option.value}>{option.label}</option>
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
         ))}
       </select>
     </fieldset>
