f886a0cc58408e8912fde8d1ff4101e55c633d2c
Bruna Romeiro <bruna_romeiro@yahoo.com.br>
2026-06-10 16:57:39 -0300

    ajusta componente Select para receber props classname, ajusta filtro para busca por estado e cidade

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -5,9 +5,17 @@ import data from "../pets.json";
 import { ageOptions, sizeOptions, species } from "./constants";
 import type { PetsList } from "./types";
 import { useFilterFields } from "./hooks/useFilterfields";
+import { districtsOptions, stateOptions } from "../common/constants";
+import { useGetDistricts } from "../common/hooks/useGetDistricts";
+import { useGetStates } from "../common/hooks/useGetStates";
 
 export default function Home() {
   const { filters, setField, reset } = useFilterFields();
+  const { states, loading } = useGetStates();
+
+  const { districts, loading: isLoading } = useGetDistricts({
+    UF: filters.state,
+  });
 
   const list = data as PetsList[];
 
@@ -16,6 +24,8 @@ export default function Home() {
     if (filters.species && pet.species !== filters.species) return false;
     if (filters.age && pet.age !== filters.age) return false;
     if (filters.size && pet.size !== filters.size) return false;
+    if (filters.state && pet.state !== filters.state) return false;
+    if (filters.city && pet.city !== filters.city) return false;
     return true;
   });
 
@@ -23,7 +33,7 @@ export default function Home() {
     <main>
       <h1 className="mb-10">Aumigos disponíveis para adoção</h1>
 
-      <form className="bg-[var(--card-bg)] shadow-[var(--shadow)] shadow-md rounded-lg p-4 mb-4">
+      <form className="bg-[var(--card-bg)] shadow-[var(--shadow)] shadow-md rounded-lg p-4 mb-8">
         <div className="flex flex-wrap gap-4 ">
           <Select
             label="Espécie:"
@@ -42,6 +52,20 @@ export default function Home() {
               onChange={(value) => setField("size", value)}
             />
           )}
+
+          <Select
+            label="Estado:"
+            options={stateOptions(states)}
+            onChange={(value) => setField("state", value)}
+            disabled={loading}
+          />
+          <Select
+            label="Cidade:"
+            options={districtsOptions(districts)}
+            onChange={(value) => setField("city", value)}
+            disabled={loading || isLoading || districts.length === 0}
+            className="min-w-[12rem]"
+          />
         </div>
 
         <button
