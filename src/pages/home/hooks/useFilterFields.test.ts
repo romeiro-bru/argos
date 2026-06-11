123e9f838c7e83cb26c3552614756a564bd8d48f
Bruna Romeiro <bruna_romeiro@yahoo.com.br>
2026-06-11 18:24:52 -0300

    cria testes para hook de form

---
+++ src/pages/home/hooks/useFilterFields.test.ts
@@ -0,0 +1,111 @@
+import { renderHook } from "@testing-library/react";
+import { useFilterFields } from "./useFilterfields";
+import { act } from "react";
+
+describe("useFilterFields", () => {
+  it("should return empty initial values", () => {
+    const { result } = renderHook(() => useFilterFields());
+
+    expect(result.current.filters).toEqual({
+      species: "",
+      age: "",
+      size: "",
+      city: "",
+      state: "",
+    });
+  });
+
+  it("should update a field", () => {
+    const { result } = renderHook(() => useFilterFields());
+
+    act(() => {
+      result.current.setField("species", "Cachorro");
+    });
+
+    expect(result.current.filters.species).toBe("Cachorro");
+  });
+
+  it("should remove size field when 'cat' is selected", () => {
+    const { result } = renderHook(() => useFilterFields());
+
+    act(() => {
+      result.current.setField("species", "Gato");
+    });
+
+    expect(result.current.filters.size).toBe("");
+  });
+
+  it("should reset all fields", () => {
+    const { result } = renderHook(() => useFilterFields());
+
+    act(() => {
+      result.current.setField("species", "Cachorro");
+      result.current.setField("city", "Rio de Janeiro");
+    });
+
+    act(() => {
+      result.current.reset();
+    });
+
+    expect(result.current.filters).toEqual({
+      species: "",
+      age: "",
+      size: "",
+      city: "",
+      state: "",
+    });
+  });
+
+  it("should filter pets by species", () => {
+    const { result } = renderHook(() => useFilterFields());
+
+    act(() => {
+      result.current.setField("species", "Cachorro");
+    });
+
+    expect(result.current.filteredList).toHaveLength(10);
+
+    expect(result.current.filteredList[0]).toMatchObject({
+      species: "Cachorro",
+    });
+  });
+
+  it("should filter pets by species and size", () => {
+    const { result } = renderHook(() => useFilterFields());
+
+    act(() => {
+      result.current.setField("species", "Cachorro");
+      result.current.setField("size", "Médio");
+    });
+
+    expect(result.current.filteredList).toHaveLength(4);
+
+    expect(result.current.filteredList[0]).toMatchObject({
+      species: "Cachorro",
+    });
+    expect(result.current.filteredList[0]).toMatchObject({
+      size: "Médio",
+    });
+  });
+
+  
+  it("should filter pets by species, size, age and state", () => {
+    const { result } = renderHook(() => useFilterFields());
+
+    act(() => {
+      result.current.setField("species", "Cachorro");
+      result.current.setField("size", "Médio");
+      result.current.setField("age", "Jovem");
+        result.current.setField("state", "RJ");
+    });
+
+    expect(result.current.filteredList).toHaveLength(2);
+
+    expect(result.current.filteredList[0]).toMatchObject({
+      species: "Cachorro",
+    });
+    expect(result.current.filteredList[0]).toMatchObject({
+      size: "Médio",
+    });
+  });
+});
