733bcd0754bdebffea06ee33bb66c083ec2d3507
Bruna Romeiro <bruna_romeiro@yahoo.com.br>
2026-06-18 12:27:35 -0300

    cria footer

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,12 +1,27 @@
 import { Outlet } from "react-router-dom";
 import { Sidebar } from "./sidebar";
-
 export default function Layout() {
   return (
     <div className="flex min-h-screen">
       <Sidebar />
-      <div className="flex-1 px-4 py-8">
-        <Outlet />
+      <div className="flex-1 flex flex-col px-4 pt-8">
+        <div className="flex-1">
+          <Outlet />
+        </div>
+        <footer className="border-t border-gray-300 mt-12 py-4">
+          <ul className="flex justify-between items-center text-xs">
+            <li className="flex flex-wrap items-center gap-2 font-semibold">
+              <img src="/favicon.png" className="h-6" />
+              Argos
+            </li>
+            <div className="flex flex-wrap gap-4">
+              <li>Sobre</li>
+              <li>Parceiros</li>
+              <li>Privacidade</li>
+              <li>Contato</li>
+            </div>
+          </ul>
+        </footer>
       </div>
     </div>
   );
