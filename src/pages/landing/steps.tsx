2b9fc5fcddd325a3df99a9d739d3cc75e8ae952c
Bruna Romeiro <bruna_romeiro@yahoo.com.br>
2026-06-18 12:36:02 -0300

    cria const para lista de itens em step

--- src/pages/landing/steps.tsx
+++ src/pages/landing/steps.tsx
@@ -2,6 +2,25 @@ import { Link } from "react-router-dom";
 import { Paw } from "../../assets/paw";
 import { appRoutes } from "../../routes";
 
+const list = [
+  {
+    title: "Cadastre-se",
+    description: "Crie seu perfil e conte seu estilo de vida",
+    icon: <img src="/user-yellow.png" className="h-5 mr-2" />,
+  },
+  {
+    title: "Encontre um match",
+    description: "Veja pets compatíveis com você",
+    icon: <Paw color="#eea900" size={35} />,
+  },
+
+  {
+    title: "Adote",
+    description: "Finalize o processo e leve para o seu lar",
+    icon: <img src="/house-yellow.png" className="h-6 mr-2" />,
+  },
+];
+
 export function Steps() {
   return (
     <section className="m-auto w-fit my-8">
@@ -9,41 +28,23 @@ export function Steps() {
         <h3 className="font-semibold">Como funciona</h3>
 
         <Link to={appRoutes.HOME.path}>
-        <span className="flex gap-2 items-center text-xs">
-          Ver todos
-          <img src="/arrow-right.png" className="h-3" />
-        </span>
+          <span className="flex gap-2 items-center text-xs">
+            Ver todos
+            <img src="/arrow-right.png" className="h-3" />
+          </span>
         </Link>
       </div>
 
       <div className="flex flex-wrap gap-2 text-[var(--subtitle)] mt-2">
-        <div className="grid border-1 border-[var(--subtitle)] px-4 py-6 rounded-lg">
-          <span className="flex font-semibold">
-            <img src="/user-yellow.png" className="h-5 mr-2" />
-            Cadastre-se
-          </span>
-          <p className="text-xs text-[var(--shadow)]">
-            Crie seu perfil e conte seu estilo de vida
-          </p>
-        </div>
-        <div className="grid border-1 border-[var(--subtitle)] px-4 py-6 rounded-lg">
-          <span className="flex font-semibold">
-            <Paw color="#eea900" size={35} />
-            Encontre um match
-          </span>
-          <p className="text-xs text-[var(--shadow)]">
-            Veja pets compatíveis com você
-          </p>
-        </div>
-        <div className="grid border-1 border-[var(--subtitle)] px-4 py-6 rounded-lg">
-          <span className="flex font-semibold">
-            <img src="/house-yellow.png" className="h-6 mr-2" />
-            Adote
-          </span>
-          <p className="text-xs text-[var(--shadow)]">
-            Finalize o processo e leve para o seu lar
-          </p>
-        </div>
+        {list.map((item) => (
+          <div className="grid border-1 border-[var(--subtitle)] px-4 py-6 rounded-lg">
+            <span className="flex font-semibold">
+              {item.icon}
+              {item.title}
+            </span>
+            <p className="text-xs text-[var(--shadow)]">{item.description}</p>
+          </div>
+        ))}
       </div>
     </section>
   );
