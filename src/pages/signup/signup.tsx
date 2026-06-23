c505e280a30ae4767345acf2a75a364bcb31f677
Bruna Romeiro <bruna_romeiro@yahoo.com.br>
2026-06-23 19:26:25 -0300

    cria form de cadastro de usuário

--- src/pages/signup/signup.tsx
+++ src/pages/signup/signup.tsx
@@ -1,5 +1,82 @@
+import { useState } from "react";
+
 export function Signup() {
-    return(
-        <h1>Signup</h1>
-    )
+  const [name, setName] = useState<string | undefined>();
+  const [email, setEmail] = useState<string | undefined>();
+  const [password, setPassword] = useState<string | undefined>();
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setLoading(true);
+
+    console.log(name, email, password)
+    // envia dados para supabase
+
+    setLoading(false);
+  };
+
+  return (
+    <main>
+      <div className="flex gap-2 mb-8 items-center">
+        <h1>Criar conta</h1>
+        <p className="flex w-fit m-auto gap-2 items-center text-xs text-[var(--subtitle)] border bg-[#fff] rounded-sm p-2">
+          <img src="/user-yellow.png" className="h-4" />É necessário criar uma
+          conta antes de cadastrar um animal para adoção.
+        </p>
+      </div>
+      <form onSubmit={handleSubmit} className="mb-8">
+        <section className="flex flex-wrap gap-4 bg-[var(--card-bg)] shadow-[var(--shadow)] shadow-md rounded-lg p-4 mb-4">
+          <fieldset>
+            <label className="flex flex-col mb-2 font-semibold" htmlFor="email">
+              E-mail:
+            </label>
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              id="email"
+              className="p-2 rounded-lg text-sm w-full"
+              required
+            />
+          </fieldset>
+          <fieldset>
+            <label className="flex flex-col mb-2 font-semibold" htmlFor="name">
+              Nome:
+            </label>
+            <input
+              onChange={(e) => setName(e.target.value)}
+              type="text"
+              id="name"
+              className="p-2 rounded-lg text-sm w-full"
+              required
+            />
+          </fieldset>
+
+          <fieldset>
+            <label
+              className="flex flex-col mb-2 font-semibold"
+              htmlFor="password"
+            >
+              Senha:
+            </label>
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              id="password"
+              minLength={6}
+              className="p-2 rounded-lg text-sm w-full"
+              required
+            />
+          </fieldset>
+        </section>
+        <button
+          disabled={loading}
+          type="submit"
+          className="cursor-pointer bg-[var(--secondary-color)] shadow-md font-semibold text-white rounded-lg py-2 px-6"
+        >
+          Criar conta
+        </button>
+      </form>
+    </main>
+  );
 }
