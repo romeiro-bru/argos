6ada9f9e60274e2cf22c7dd85d0bae23a308377f
Bruna Romeiro <bruna_romeiro@yahoo.com.br>
2026-06-27 00:49:05 -0300

    add componente spinner

---
+++ src/assets/spinner.tsx
@@ -0,0 +1,28 @@
+type SpinnerProps = {
+  className?: string;
+};
+
+export function Spinner({ className = "size-5" }: SpinnerProps) {
+  return (
+    <svg
+      className={`animate-spin ${className}`}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle
+        className="opacity-25"
+        cx="12"
+        cy="12"
+        r="10"
+        stroke="currentColor"
+        strokeWidth="4"
+      />
+      <path
+        className="opacity-75"
+        fill="currentColor"
+        d="M4 12a8 8 0 0 1 8-8V0C5.373 0 0 5.373 0 12h4z"
+      />
+    </svg>
+  );
+}
