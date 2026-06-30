a5f8ffaf8dbde412d8ba68907a98823e181ab900
Bruna Romeiro <bruna_romeiro@yahoo.com.br>
2026-06-30 12:49:33 -0300

    cria provider para dados de sessão do usuário

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,23 @@ import { appRoutes } from "./routes";
 import Registration from "./pages/registration/registration";
 import Landing from "./pages/landing/landing";
 import { Signup } from "./pages/signup/signup";
+import { UserSupabaseProvider } from "./context/userSupabaseContext";
 
 export default function App() {
   return (
-    <FavoritesProvider>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route index element={<Landing />} />
-          <Route path={appRoutes.HOME.path} element={<Home />} />
-          <Route path={appRoutes.FAVORITES.path} element={<Favorites />} />
-          <Route path={appRoutes.DETAILS.path} element={<Details />} />
-          <Route path={appRoutes.REGISTER.path} element={<Registration />} />
-          <Route path={appRoutes.SIGN_UP.path} element={<Signup />} />
-        </Route>
-      </Routes>
-    </FavoritesProvider>
+    <UserSupabaseProvider>
+      <FavoritesProvider>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route index element={<Landing />} />
+            <Route path={appRoutes.HOME.path} element={<Home />} />
+            <Route path={appRoutes.FAVORITES.path} element={<Favorites />} />
+            <Route path={appRoutes.DETAILS.path} element={<Details />} />
+            <Route path={appRoutes.REGISTER.path} element={<Registration />} />
+            <Route path={appRoutes.SIGN_UP.path} element={<Signup />} />
+          </Route>
+        </Routes>
+      </FavoritesProvider>
+    </UserSupabaseProvider>
   );
 }
--- src/routes.tsx
+++ src/routes.tsx
@@ -40,12 +40,12 @@ export const appRoutes = {
       showInSidebar: true,
     },
   },
-
+  
   SIGN_UP: {
     path: "/sign-up",
     meta: {
-      label: "Criar usuários",
-      showInSidebar: false
-    }
-  }
+      label: "Minha conta",
+      showInSidebar: false,
+    },
+  },
 };
