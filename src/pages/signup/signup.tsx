4768b964b75532eb13dc6e88bf54c7e1336bc614
Bruna Romeiro <bruna_romeiro@yahoo.com.br>
2026-07-01 19:17:04 -0300

    não permite usuário acessar form de cadastro se já estiver logado

--- src/pages/signup/signup.tsx
+++ src/pages/signup/signup.tsx
@@ -8,6 +8,8 @@ import { useNavigate } from "react-router-dom";
 import { ErrorModal } from "../../components/modalError";
 import { useAuthForm } from "./useAuthForm";
 import { AuthFormSkeleton } from "./authFormSkeleton";
+import { useUserSupabase } from "../../context/userSupabaseContext";
+import { WarningTag } from "../../components/warningTag";
 
 export interface FormDataInterface {
   name: string;
@@ -33,79 +35,91 @@ export function Signup() {
 
   const isLogin = mode === "login";
 
-  // TODO: se usuário já estiver logado, não permitir logar novamente
+  const { session } = useUserSupabase();
 
   return (
     <main>
-      <div className="flex gap-2 mb-8 items-center">
-        <h1>Criar conta</h1>     
-      </div>
-      <form onSubmit={handleSubmit} className="mb-8">
-        <section className="flex flex-wrap gap-4 bg-[var(--card-bg)] shadow-[var(--shadow)] shadow-md rounded-lg p-4 mb-4">
-          {loading ? (
-            <AuthFormSkeleton showName={!isLogin} />
-          ) : (
-            <>
-              <EmailField formData={formData} setFormData={setFormData} />
-              {!isLogin && (
-                <NameField formData={formData} setFormData={setFormData} />
-              )}
-              <PasswordField formData={formData} setFormData={setFormData} />
-            </>
-          )}
+      {session ? (
+        <section>
+          <h1 className="mb-8">Usuário já autenticado.</h1>
+          <WarningTag message="Para cadastrar nova conta é necessário realizar logout." />
         </section>
+      ) : (
+        <>
+          <form onSubmit={handleSubmit} className="mb-8">
+            <section className="flex flex-wrap gap-4 bg-[var(--card-bg)] shadow-[var(--shadow)] shadow-md rounded-lg p-4 mb-4">
+              {loading ? (
+                <AuthFormSkeleton showName={!isLogin} />
+              ) : (
+                <>
+                  <div className="flex gap-2 mb-8 items-center">
+                    <h1>Criar conta</h1>
+                  </div>
+                  <EmailField formData={formData} setFormData={setFormData} />
+                  {!isLogin && (
+                    <NameField formData={formData} setFormData={setFormData} />
+                  )}
+                  <PasswordField
+                    formData={formData}
+                    setFormData={setFormData}
+                  />
+                </>
+              )}
+            </section>
 
-        <div className="flex gap-2">
-          <button
-            disabled={loading}
-            onClick={toggleMode}
-            type="button"
-            className="cursor-pointer border-2 border-[var(--secondary-color)] text-[var(--secondary-color)] shadow-md rounded-lg py-2 px-4 mr-2"
-          >
-            Trocar para {isLogin ? "Criar conta" : "Login"}
-          </button>
-          <button
-            disabled={loading}
-            type="submit"
-            className="flex gap-2  items-center cursor-pointer bg-[var(--secondary-color)] shadow-md font-semibold text-white rounded-lg py-2 px-6"
-          >
-            {loading && <Spinner className="size-4" />}
+            <div className="flex gap-2">
+              <button
+                disabled={loading}
+                onClick={toggleMode}
+                type="button"
+                className="cursor-pointer border-2 border-[var(--secondary-color)] text-[var(--secondary-color)] shadow-md rounded-lg py-2 px-4 mr-2"
+              >
+                Trocar para {isLogin ? "Criar conta" : "Login"}
+              </button>
+              <button
+                disabled={loading || !!session}
+                type="submit"
+                className="flex gap-2  items-center cursor-pointer bg-[var(--secondary-color)] shadow-md font-semibold text-white rounded-lg py-2 px-6"
+              >
+                {loading && <Spinner className="size-4" />}
 
-            {isLogin ? "Login" : "Criar conta"}
-          </button>
-        </div>
+                {isLogin ? "Login" : "Criar conta"}
+              </button>
+            </div>
 
-        <SuccessModal
-          isOpen={showSuccess}
-          onClose={() => {
-            setShowSuccess(false);
-            navigate(appRoutes.REGISTER.path);
-          }}
-          title={isLogin ? "Login realizado!" : "Conta criada!"}
-          message={
-            isLogin
-              ? "Login realizado com sucesso."
-              : "Uma mensagem foi enviada para o seu e-mail, após a confirmação você poderá cadastrar um animal para adoção."
-          }
-          actionLabel="Continuar"
-          onAction={() => navigate(appRoutes.REGISTER.path)}
-        />
-        <ErrorModal
-          isOpen={showError}
-          onClose={() => {
-            setShowError(false);
-            navigate(appRoutes.SIGNUP.path);
-          }}
-          title={
-            isLogin
-              ? "Erro ao autenticar"
-              : "Não foi possível criar a sua conta."
-          }
-          onAction={() => navigate(appRoutes.LANDING.path)}
-          actionLabel="Continuar"
-          message={errorMessage}
-        />
-      </form>
+            <SuccessModal
+              isOpen={showSuccess}
+              onClose={() => {
+                setShowSuccess(false);
+                navigate(appRoutes.REGISTER.path);
+              }}
+              title={isLogin ? "Login realizado!" : "Conta criada!"}
+              message={
+                isLogin
+                  ? "Login realizado com sucesso."
+                  : "Uma mensagem foi enviada para o seu e-mail, após a confirmação você poderá cadastrar um animal para adoção."
+              }
+              actionLabel="Continuar"
+              onAction={() => navigate(appRoutes.REGISTER.path)}
+            />
+            <ErrorModal
+              isOpen={showError}
+              onClose={() => {
+                setShowError(false);
+                navigate(appRoutes.SIGNUP.path);
+              }}
+              title={
+                isLogin
+                  ? "Erro ao autenticar"
+                  : "Não foi possível criar a sua conta."
+              }
+              onAction={() => navigate(appRoutes.LANDING.path)}
+              actionLabel="Continuar"
+              message={errorMessage}
+            />
+          </form>
+        </>
+      )}
     </main>
   );
 }
