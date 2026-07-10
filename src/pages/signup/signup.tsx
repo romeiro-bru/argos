facd30c6ddb98276e7b7ae200c122f2052493e54
Bruna Romeiro <bruna_romeiro@yahoo.com.br>
2026-07-10 16:00:39 -0300

    muda title condicionalmente

--- src/pages/signup/signup.tsx
+++ src/pages/signup/signup.tsx
@@ -47,7 +47,7 @@ export function Signup() {
       ) : (
         <>
           <div className="flex gap-2 mb-8 items-center">
-            <h1>Criar conta</h1>
+            <h1>{isLogin ? "Fazer Login" : "Criar conta"}</h1>
           </div>
           <form onSubmit={handleSubmit} className="mb-8">
             <section className="flex flex-wrap gap-4 bg-[var(--card-bg)] shadow-[var(--shadow)] shadow-md rounded-lg p-4 mb-4">
