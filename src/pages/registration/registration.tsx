92e748a3203fc947ab6b0c11fbb3d63a3cff0693
Bruna Romeiro <bruna_romeiro@yahoo.com.br>
2026-07-14 16:38:01 -0300

    passa service hooks para mesmo arquivo

--- src/pages/registration/registration.tsx
+++ src/pages/registration/registration.tsx
@@ -4,29 +4,70 @@ import { Form } from "./components/form";
 import { UserNotAllowed } from "./components/userNotAllowed";
 import { ErrorModal } from "../../components/modalError";
 import { SuccessModal } from "../../components/modalSuccess";
-import { useRegistrationSubmit } from "./hooks/useRegistrationSubmit";
 import { appRoutes } from "../../routes";
+import { useState } from "react";
+import {
+  useNewPetService,
+  useUploadImgService,
+} from "./hooks/useNewPetService";
+import type { FormState } from "./types";
+
+interface HandleSubmitParams {
+  e: React.FormEvent<HTMLFormElement>;
+  formState: FormState;
+  validateForm: () => boolean;
+}
 
 export default function Registration() {
   const navigate = useNavigate();
+  const [showSuccess, setShowSuccess] = useState(false);
+  const [showError, setShowError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   const { session } = useUserSupabase();
 
-  const {
-    handleSubmit,
-    isPending,
-    isUploadingImg,
-    showSuccess,
-    setShowSuccess,
-    showError,
-    setShowError,
-    errorMessage,
-    setErrorMessage,
-  } = useRegistrationSubmit({ userId: session?.user.id ?? "" });
+  const { mutateAsync: uploadImg, isPending: isUploadingImg } =
+    useUploadImgService();
+  const { mutateAsync: createPet, isPending: isCreatingPet } =
+    useNewPetService();
+
+  const registerPet = async (formState: FormState) => {
+    const imageUrl = await uploadImg(formState.image!);
+
+    const { image, ...fields } = formState;
+
+    // monta payload com url retornada(string), excluindo image (file)
+    return createPet({
+      pet: {
+        ...fields,
+        imageUrl,
+        user_id: session!.user.id ?? "",
+      },
+    });
+  };
+  const handleSubmit = async ({
+    e,
+    formState,
+    validateForm,
+  }: HandleSubmitParams) => {
+    e.preventDefault();
+
+    if (!validateForm()) return;
+    if (!formState.image) return;
+
+    try {
+      await registerPet(formState);
+
+      setShowSuccess(true);
+    } catch (err) {
+      setErrorMessage(err instanceof Error ? err.message : "Erro inesperado.");
+      setShowError(true);
+    }
+  };
+
+  const isLoadingRegister = isCreatingPet || isUploadingImg;
 
   if (!session) return <UserNotAllowed />;
-
-  const isLoadingRegister = isPending || isUploadingImg
 
   return (
     <main>
